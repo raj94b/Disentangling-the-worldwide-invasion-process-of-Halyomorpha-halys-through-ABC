40580085fe53d8a195a87a4b3fab6fca7c7916d9
Rajiv Boscolo Agostini <134699560+raj94b@users.noreply.github.com>
2024-06-25 15:12:25 +0200

    Update model_selection_step3.R

--- Model_selection/model_selection_step3.R
+++ Model_selection/model_selection_step3.R
@@ -9,5 +9,4 @@ f<-apply(s,2,var)!=0
 da<-data.frame(i,s[,f])
 obs<-obs[,f]
 a<-abcrf(i~.,data=da,lda=T,ntree=ntrees,paral=T,ncores=8)
-plot(a, training=da, obs=obs, pdf=TRUE)
 b<-predict(object=a,obs=rbind(obs,obs),training=da,ntree=ntrees,paral=T,paral.predict=T,ncores=4,ncores.predict=4)
